d6cd5483bb22928041f0fb3a6313a8300ed8a965
Dasun Thamash <tunathamash@gmail.com>
2023-09-17 00:50:50 +0530

    sole exceptions

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,24 @@
 
+const dashBoardPage=()=>{
+    console.log("running!")
+
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email,password)
+        .then((resp)=>{
+            console.log(resp);
+            console.log(resp.user);
+            window.location.href="dashboard.html";
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+
+}
+
 const createAnAccount=()=>{
     window.location.href="register.html";
 }
