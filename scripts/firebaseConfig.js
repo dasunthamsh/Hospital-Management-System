b5be2593b2a4403da406cc075f0f1dbfa9e8f77e
Dasun Thamash <tunathamash@gmail.com>
2023-09-16 17:25:25 +0530

    firebase sdk added

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCg_ZhnCZipgMrSCLDWGpvir8vCjPGGz0",
+    authDomain: "hospital-system-f81da.firebaseapp.com",
+    projectId: "hospital-system-f81da",
+    storageBucket: "hospital-system-f81da.appspot.com",
+    messagingSenderId: "441796323736",
+    appId: "1:441796323736:web:33d86f7881172d21b223ed",
+    measurementId: "G-132QZJ84YC"
+};
+
+firebase.initializeApp(firebaseConfig);
