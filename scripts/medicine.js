164526db140d6760bbe3bb08e35d0e8ff852b814
Dasun Thamash <tunathamash@gmail.com>
2023-09-19 15:29:39 +0530

    get all medicine to table function complete

--- scripts/medicine.js
+++ scripts/medicine.js
@@ -25,3 +25,23 @@ saveMedicine=()=>{
 
 // get doctors values from firestore
 
+
+const loadMedicine=()=>{
+
+    $("#tblItemmedicine").empty();
+
+    const firestore = firebase.firestore();
+    firestore.collection('medicines').get().then((result)=>{
+        result.forEach((recodes)=>{
+            const data =recodes.data();
+            const row = `<tr><td>${recodes.code}</td><td>${data.description}</td><td>${data.qyt}</td><td>${data.price}</td><td>
+
+            <button class="bg-danger" onclick="deleteData('${recodes.code}')">Delete</button>
+            <button class="bg-success" onclick="updateData('${recodes.code}')">Update</button>
+</td></tr>`
+
+            $("#tblItemmedicine").append(row);
+        });
+    })
+
+}
