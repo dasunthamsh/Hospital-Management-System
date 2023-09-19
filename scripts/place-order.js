76a4a194e36256e741e93d3f20ebee141172f82b
Dasun Thamash <tunathamash@gmail.com>
2023-09-19 12:56:24 +0530

    add medicine page

--- scripts/place-order.js
+++ scripts/place-order.js
@@ -9,8 +9,28 @@ firestore.collection('patients').get().then((recodes =>{
     recodes.forEach((resault)=>{
         const option = $('<option></option>').val(resault.id).text(resault.id);
         $("#patient-id").append(option);
-    })
-}))
-
+    });
+}));
 
 }
+
+
+// when click patient id load the all patient data to inputs
+
+$("#patient-id").on("change",function (){
+
+const patientid = $(this).val()
+
+    const firestore = firebase.firestore();
+firestore.collection('patients').doc(patientid).get().then((response)=>{
+    if (response.exists){
+        const data = response.data();
+
+
+        $("#patient-name").val(data.name)
+        $("#patient-number").val(data.number)
+        $("#patient-address").val(data.address)
+    }
+});
+});
+
